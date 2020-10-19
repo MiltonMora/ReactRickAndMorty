007549a7e70f9842b51df068d8ec47c0d49e6bab
MiltonMora <mayloevan@gmail.com>
2020-10-19 20:34:16 +0200

    locations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layaut from './components/Layaut';
-import Characters from './components/Characters.jsx';
+import Characters from './pages/Characters.jsx';
+import Locations from './pages/Locations.jsx';
 import NotFound from './components/NotFound.jsx';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <BrowserRouter>
         <Layaut/>
         <Switch>
-          <Route exact path="/" component={ Characters} />
+          <Route exact path="/" component={ Characters } />
+          <Route exact path="/locations" component={ Locations } />
           <Route component={ NotFound } />
         </Switch>
       </BrowserRouter>
